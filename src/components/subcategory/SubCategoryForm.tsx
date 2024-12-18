02e1a0239e39d5eda6f1f0c279b4445fe7fa591f
Abdulllah321 <abdullahsufyan2007@gmail.com>
2024-12-19 00:02:32 +0500

    solved deployment problems

--- src/components/subcategory/SubCategoryForm.tsx
+++ src/components/subcategory/SubCategoryForm.tsx
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   createSubCategory,
   fetchMainCategories,
-  updateMainCategory,
   updateSubCategory,
 } from "../../slices/categorySlice";
 import { generateSlug } from "../../constants";
